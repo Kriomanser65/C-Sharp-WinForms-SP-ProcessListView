c08e30ae8018534a33e1c662bfa66e29590c16cc
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-11-22 17:46:42 +0200

    Add project files.

---
+++ Sp_ListView/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Sp_ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(291, 482);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(309, 18);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(247, 482);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(562, 18);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(226, 482);
+            this.listView3.TabIndex = 2;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.SelectedIndexChanged += new System.EventHandler(this.listView3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 512);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+    }
+}
+
---
+++ Sp_ListView/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Sp_ListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateProcessList();
+        }
+        private void PopulateProcessList()
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("PID", 70);
+            listView1.Columns.Add("Name", 150);
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.Id.ToString());
+                item.SubItems.Add(process.ProcessName);
+                listView1.Items.Add(item);
+            }
+        }
+        private void listViewProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                int processId = int.Parse(selectedItem.Text);
+                try
+                {
+                    Process selectedProcess = Process.GetProcessById(processId);
+                    DisplayThreads(selectedProcess);
+                    DisplayModules(selectedProcess);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void DisplayThreads(Process process)
+        {
+            listView2.Items.Clear();
+            listView2.View = View.Details;
+            listView2.Columns.Add("ID", 50);
+            listView2.Columns.Add("Priority", 70);
+            listView2.Columns.Add("Start Time", 120);
+            try
+            {
+                ProcessThreadCollection threads = process.Threads;
+                foreach (ProcessThread thread in threads)
+                {
+                    ListViewItem item = new ListViewItem(thread.Id.ToString());
+                    item.SubItems.Add(thread.PriorityLevel.ToString());
+                    item.SubItems.Add(thread.StartTime.ToString());
+                    listView2.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error fetching threads: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void DisplayModules(Process process)
+        {
+            listView3.Items.Clear();
+            listView3.View = View.Details;
+            listView3.Columns.Add("Name", 200);
+            listView3.Columns.Add("Path", 300);
+            try
+            {
+                ProcessModuleCollection modules = process.Modules;
+                foreach (ProcessModule module in modules)
+                {
+                    ListViewItem item = new ListViewItem(module.ModuleName);
+                    item.SubItems.Add(module.FileName);
+                    listView3.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error fetching modules: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
